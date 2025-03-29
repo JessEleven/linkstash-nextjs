98161549b5d290dab84d2ccda3dcb32fe905ace4
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-03-29 00:25:27 -0500

    bug fixes with Better Auth and drizzle-kit settings

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -1,5 +1,5 @@
-import { db } from '@/db/drizzle'
-import { schema } from '@/db/schema'
+import { db } from '../db/drizzle'
+import { schema } from '../db/schema'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
@@ -7,11 +7,12 @@ import { nextCookies } from 'better-auth/next-js'
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg', // or "mysql", "sqlite"
-    schema: schema
+    schema
   }),
   emailAndPassword: {
     enabled: true,
-    autoSignIn: false
+    // After creating the account redirect, e.g. '/dashboard'
+    autoSignIn: true
   },
   plugins: [nextCookies()]
 })

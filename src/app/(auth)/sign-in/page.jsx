05390958cd993edc9535c1cd5e4142a3ed2ab533
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-03-30 00:50:15 -0500

    UX enhancement with the login and registration forms

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,33 +1,85 @@
-import AppLogo from '@/components/app-logo'
+'use client'
+
+import AppLogo from '@/components/UI/app-logo'
+import { authClient } from '@/libs/auth-client'
+import { EyeIcon, EyeOffIcon } from '@/resources/assets/auth-icons'
+import { LoaderIcon } from '@/resources/assets/global-icons'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState, useTransition } from 'react'
 
 export default function SignInPage () {
+  const [isPending, startTransition] = useTransition()
+  const [isHide, setIsHide] = useState(true)
+  const [formData, setFormData] = useState({
+    email: '', password: ''
+  })
+  const router = useRouter()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    startTransition(async () => {
+      await authClient.signIn.email(formData, {
+        onRequest: () => {
+          console.log('Request sent...')
+        },
+        onSuccess: () => {
+          console.log('Sign In successful!')
+          router.push('/dashboard')
+        },
+        onError: (ctx) => {
+          console.log('Error:', ctx)
+        }
+      })
+    })
+  }
+
   return (
     <main className='flex place-content-center'>
       <div className='flex items-center justify-center h-screen w-full md:w-[372px] mx-7 md:mx-0'>
-        <form action='' className='flex flex-col space-y-4 text-sm w-full'>
+        <form onSubmit={handleSubmit} className='flex flex-col space-y-4 text-sm w-full'>
           <Link translate='no' href='/' className='flex items-center justify-center gap-x-1.5'>
             <AppLogo />
           </Link>
           <h3 className='text-base'>Sign in to your account</h3>
           <div className='flex flex-col'>
-            <label htmlFor='' className='mb-1.5'>Email</label>
+            <label htmlFor='email' className='mb-1.5'>Email</label>
             <input
+              id='email'
               type='email'
+              name='email'
               placeholder='you@example.com'
               className='input'
+              value={formData.email}
+              onChange={handleChange}
+              required
             />
           </div>
           <div className='flex flex-col'>
-            <label htmlFor='' className='mb-1.5'>Password</label>
-            <input
-              type='password'
-              placeholder='••••••••'
-              className='input'
-            />
+            <label htmlFor='password' className='mb-1.5'>Password</label>
+            <div className='relative flex items-center text-neutral-500 focus-within:text-slate-500'>
+              <input
+                id='password'
+                type={isHide ? 'password' : 'text'}
+                name='password'
+                placeholder='••••••••'
+                className='input w-full text-neutral-50'
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+              <button type='button' aria-label='View password' className='absolute right-4 cursor-pointer' onClick={() => setIsHide(!isHide)}>
+                {isHide ? <EyeIcon /> : <EyeOffIcon />}
+              </button>
+            </div>
           </div>
-          <button className='button'>
-            Sign In
+          <button className='btn-session flex items-center justify-center gap-x-0.5'>
+            {isPending ? <><LoaderIcon className='size-5 animate-spin' /> <span>Signing In...</span> </> : 'Sign In'}
           </button>
         </form>
       </div>
---
+++ src/resources/assets/auth-icons.jsx
@@ -0,0 +1,35 @@
+export const EyeIcon = (props) => (
+  <svg
+    width={20}
+    height={20}
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth={2}
+    {...props}
+  >
+    <path stroke='none' d='M0 0h24v24H0z' />
+    <path d='M10 12a2 2 0 1 0 4 0 2 2 0 0 0-4 0' />
+    <path d='M21 12c-2.4 4-5.4 6-9 6-3.6 0-6.6-2-9-6 2.4-4 5.4-6 9-6 3.6 0 6.6 2 9 6' />
+  </svg>
+)
+
+export const EyeOffIcon = (props) => (
+  <svg
+    width={20}
+    height={20}
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth={2}
+    {...props}
+  >
+    <path stroke='none' d='M0 0h24v24H0z' />
+    <path d='M10.585 10.587a2 2 0 0 0 2.829 2.828' />
+    <path d='M16.681 16.673A8.717 8.717 0 0 1 12 18c-3.6 0-6.6-2-9-6 1.272-2.12 2.712-3.678 4.32-4.674m2.86-1.146A9.055 9.055 0 0 1 12 6c3.6 0 6.6 2 9 6-.666 1.11-1.379 2.067-2.138 2.87M3 3l18 18' />
+  </svg>
+)
